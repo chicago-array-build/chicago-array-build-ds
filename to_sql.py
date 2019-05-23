aa1f3ca10115f599486a8689e6a5989e2102505e
macscheffer <43249742+macscheffer@users.noreply.github.com>
2019-05-23 11:52:36 -0400

    Create to_sql.py

---
+++ to_sql.py
@@ -0,0 +1,17 @@
+import tarfile
+import pandas as pd
+
+
+file = 'Downloads/chicago-complete.weekly.2019-04-15-to-2019-04-21.tar'
+
+tf = tarfile.TarFile(file)
+
+# this saves the csv to a folder within the repo
+tf.extract(tf.getnames()[-2], path='AoT_test2.csv')
+
+# the path will be the csv within the saved folder from above.
+weekly = pd.read_csv('AoT_test2.csv/chicago-complete.weekly.2019-04-15-to-2019-04-21/data.csv.gz')
+
+# reading into pandas. 
+weekly['sensor_path'] = weekly.subsystem + '.' + weekly.sensor + '.' + weekly.parameter
+observations = weekly[['timestamp', 'node_id', 'sensor_path', 'value_hrf']]
